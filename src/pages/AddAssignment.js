43bbc6ec1277a39b3c1064a926350fb4712bce15
LCL262 <24037880@myrp.edu.sg>
2026-01-29 12:42:41 +0800

    Update AddAssignment.js

--- src/pages/AddAssignment.js
+++ src/pages/AddAssignment.js
@@ -1,7 +1,7 @@
 import {useEffect, useState} from "react";
 import { useNavigate } from "react-router-dom";
 import AssignmentForm from "../components/AssignmentForm";
-import { addCard } from "../services/api"; // Keep as addCard
+import { addAssignment } from "../services/api"; 
 
 export default function AddAssignment() {
     const navigate = useNavigate();
